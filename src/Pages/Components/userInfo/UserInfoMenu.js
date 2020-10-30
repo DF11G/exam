4d0a66acef6a9732f5df3ba69d1d589d4f773347
warthog <853996234hu@gmail.com>
2020-10-30 21:09:38 +0800

    fix

--- src/Pages/Components/userInfo/UserInfoMenu.js
+++ src/Pages/Components/userInfo/UserInfoMenu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { withRouter, createBrowserHistory } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import Axios from 'axios'
 
 import { Menu, Dropdown } from 'antd';
