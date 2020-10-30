eac802a71b84b0157ba80f2fc98b060a7a072544
warthog <853996234hu@gmail.com>
2020-10-30 16:16:31 +0800

    add change

--- src/Pages/Page.js
+++ src/Pages/Page.js
@@ -1,19 +1,54 @@
 import React, { Component } from 'react'
-import Main from './Main'
-import { BrowserRouter, Route } from 'react-router-dom'
-import Font from './Font'
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
+
+import { Layout, Menu, Breadcrumb } from 'antd';
+import "antd/dist/antd.css"
+
+import Login from './Components/Index/Login'
+import Register from './Components/Index/Register'
+import ChangePassword from './Components/Index/ChangePassword'
+
+import './Page.css'
+
+const { Header, Content, Footer } = Layout;
 
 class Page extends Component {
     render() {
         return (
             <BrowserRouter>
                 <div>
-                    <Route
-                        path='/' component={Font}
-                    ></Route>
-                    <Route
-                        path='/main' component={Main}
-                    ></Route>
+                    <Layout className="layout">
+                        <Header>
+                        <div className="logo" />
+                        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                            <Menu.Item key="1">nav 1</Menu.Item>
+                            <Menu.Item key="2">nav 2</Menu.Item>
+                            <Menu.Item key="3">nav 3</Menu.Item>
+                        </Menu>
+                        </Header>
+                        <Content style={{ padding: '0 50px' }}>
+                        <Breadcrumb style={{ margin: '16px 0' }}>
+                            <Breadcrumb.Item>Home</Breadcrumb.Item>
+                            <Breadcrumb.Item>List</Breadcrumb.Item>
+                            <Breadcrumb.Item>App</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <div className="site-layout-content">
+                            <Switch>
+                                <Route
+                                    path='/login' component={Login}
+                                ></Route>
+                                <Route
+                                    path='/register' component={Register}
+                                ></Route>
+                                <Route
+                                    path='/changePassword' component={ChangePassword}
+                                ></Route>
+                                <Redirect exact to="/login" from='/' />
+                            </Switch>
+                        </div>
+                        </Content>
+                        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                    </Layout>
                 </div>
             </BrowserRouter>
         )
